b293da8274e0f6a703dbed23854d6a0c32ea1d7a
TechWizard-SS <samir898877.s@gmail.com>
2025-11-04 18:03:05 +0400

    Написал небольшую докудобавил зависимости, которых н е хватало лдля проверки покрытия тестами

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     application
     checkstyle
     id("org.sonarqube") version "7.0.1.6134"
+    jacoco
 }
 
 group = "hexlet.code"
@@ -26,6 +27,30 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.5".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
 
 tasks.checkstyleMain {
@@ -47,5 +72,14 @@ sonar {
     properties {
         property("sonar.projectKey", "TechWizard-SS_java-project-78")
         property("sonar.organization", "techwizard-ss")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "src/main")
+        property("sonar.tests", "src/test")
+        property("sonar.java.binaries", "build/classes/java/main")
+        property("sonar.java.test.binaries", "build/classes/java/test")
+        property("sonar.java.test.reportPaths", "build/test-results/test")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+        property("sonar.exclusions", "**/build/**,**/generated/**")
+        property("sonar.verbose", "true")
     }
 }
