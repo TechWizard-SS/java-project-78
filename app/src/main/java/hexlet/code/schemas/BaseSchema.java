1faa54809bec76ad59ae74039125ddb66031e1d9
TechWizard-SS <samir898877.s@gmail.com>
2025-11-04 16:33:49 +0400

    Написал небольшую документацию как того требует Ñcheckstyle

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,12 +1,29 @@
 package hexlet.code.schemas;
 
+/**
+ * Базовый класс для всех схем валидации.
+ * Содержит общие методы и флаг обязательности значения.
+ *
+ * @param <T> тип данных, с которым работает схема
+ */
 public abstract class BaseSchema<T> {
     protected boolean required = false;
 
+    /**
+     * Делает поле обязательным для проверки.
+     *
+     * @return текущий объект схемы
+     */
     public BaseSchema<T> required() {
         this.required = true;
         return this;
     }
 
+    /**
+     * Проверяет корректность значения в зависимости от типа схемы.
+     *
+     * @param value значение для проверки
+     * @return true, если значение корректно, иначе false
+     */
     public abstract boolean isValid(Object value);
 }
